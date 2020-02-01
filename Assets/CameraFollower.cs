957593274788e3d81153b12bfc7ca265858c5542
DESKTOP-HCL12A0\Kyle Tanael <justinkyletanael@yahoo.com>
2020-02-01 15:33:29 +0800

    ab

---
+++ Assets/CameraFollower.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    public float dampTime;
+
+    private Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPos = GameManager.instance.player.position;
+        Vector3 point = gameObject.GetComponent<Camera>().WorldToViewportPoint(playerPos);
+        Vector3 delta = playerPos - gameObject.GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+        Vector3 destination = transform.position + delta;
+
+        transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+    }
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform player;
+    public Camera cam;
+    public int materialAmount;
+    public int neededAmount;
+    public Text materialText;
+    public string materialName;
+
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        materialAmount = 0;
+        neededAmount = 3;
+        materialText.text = string.Concat(materialName, ": ", materialAmount.ToString());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    public void AddMaterial()
+    {
+        materialAmount++;
+        UpdateProgress();
+    }
+
+
+    public void UpdateProgress()
+    {
+        materialText.text = string.Concat(materialName, ": ", materialAmount.ToString());
+    }
+}
